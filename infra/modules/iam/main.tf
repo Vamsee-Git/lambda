60f208ac17d0fcd5a3a86ec81377a53ce071b396
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-27 10:30:29 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -20,6 +20,10 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "xray_write_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
 
 resource "aws_iam_policy" "lambda_logging" {
   name        = "LambdaLoggingPolicy"
